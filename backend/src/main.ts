a1fb5d3551cb344c049936c95228599139030e46
karthik-pv <karthik.pv77@gmail.com>
2024-07-07 11:05:30 +0530

    final commit

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,12 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { CorsMiddleware } from './middleware/CORSMiddleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.use(CorsMiddleware);
-
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
