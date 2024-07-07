3841b8c17cde5444efc7c2674c8f051e236c4477
karthik-pv <karthik.pv77@gmail.com>
2024-07-07 09:51:22 +0530

    deployment test commit

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,9 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*',
-    methods: 'GET,POST,PUT,DELETE,OPTIONS',
-    allowedHeaders: 'Content-Type,Authorization',
+    origin: 'http://localhost:3001',
+    methods: 'GET, POST, PUT, DELETE, OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
   });
   await app.listen(3000);
 }
