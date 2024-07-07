80e99b669d30b01c3ce38d1a048af8d9eadcdc71
karthik-pv <karthik.pv77@gmail.com>
2024-07-07 10:09:51 +0530

    deployment grind

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: 'http://localhost:3001',
-    methods: 'GET, POST, PUT, DELETE, OPTIONS',
+    methods: 'GET, POST, PUT, DELETE',
     allowedHeaders: 'Content-Type, Authorization',
   });
   await app.listen(3000);
