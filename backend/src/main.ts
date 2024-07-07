c68aba91310328a64d7d03e30e6fa02bf15ef3ff
karthik-pv <karthik.pv77@gmail.com>
2024-07-07 10:24:57 +0530

    CORS middleware

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,14 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsMiddleware } from './middleware/CORSMiddleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: 'http://localhost:3001',
-    methods: 'GET, POST, PUT, DELETE',
-    allowedHeaders: 'Content-Type, Authorization',
-    credentials: true,
-  });
+
+  app.use(CorsMiddleware);
+
   await app.listen(3000);
 }
 bootstrap();
---
+++ backend/src/middleware/CORSMiddleware.ts
@@ -0,0 +1,21 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+
+@Injectable()
+export class CorsMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3001');
+    res.header(
+      'Access-Control-Allow-Methods',
+      'GET, POST, PUT, DELETE, OPTIONS',
+    );
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', 'true');
+
+    if (req.method === 'OPTIONS') {
+      res.sendStatus(200);
+    } else {
+      next();
+    }
+  }
+}
